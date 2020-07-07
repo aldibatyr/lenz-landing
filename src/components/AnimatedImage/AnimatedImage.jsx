f4415a88f0b8eadb43deb496feb76dd5cd59682d
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2020-07-07 16:32:39 -0700

    cleared warnings

--- src/components/AnimatedImage/AnimatedImage.jsx
+++ src/components/AnimatedImage/AnimatedImage.jsx
@@ -1,14 +1,12 @@
-import React, { useRef, useState, useLayoutEffect } from "react";
+import React from "react";
 import dijon1 from "../../assets/Dijon+2.jpg";
-import dijon2 from "../../assets/Dijon+4.jpg";
-import dijon3 from "../../assets/Dijon+5.jpg";
+// import dijon2 from "../../assets/Dijon+4.jpg";
+// import dijon3 from "../../assets/Dijon+5.jpg";
 import "./AnimatedImage.scss";
 import { useEffect } from "react";
 import gsap from "gsap";
 
 const AnimatedImage = () => {
-  const [width, setWidth] = useState(0);
-  let parent = useRef(null);
 
   const animatedImageTimeline = gsap.timeline();
 
@@ -28,10 +26,10 @@ const AnimatedImage = () => {
         "-=0.4"
       )
       .to(".actualImage", { scale: 1.3 }, '-=0.6');
-  }, []);
+  }, [animatedImageTimeline]);
 
   return (
-    <div ref={parent} className="backgroundImageContaner">
+    <div className="backgroundImageContaner">
       <div className="solidColor"></div>
       <div className="backgroundImage">
         <img className="actualImage" src={dijon1} alt="decorative background" />
